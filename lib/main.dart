b1e72dd2512a8f0d2a1b3a22234567449812d304
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2023-08-10 22:54:29 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:e_commerce_app/firebase_options.dart';
 import 'package:e_commerce_app/screens/splash_screen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
